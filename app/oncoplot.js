ee395fec155afece1c641ab3e5296931369007a3
Sam El-Kamand <sam.elkamand@gmail.com>
2023-08-18 00:18:28 +1000

    fix: small code formatting changes

--- app/oncoplot.js
+++ app/oncoplot.js
@@ -1,5 +1,10 @@
 import * as d3 from "d3";
-import { scaleBandFacet, renderAxisX, renderAxisY, renderFacetsY } from "./scaleBandFacet.js";
+import {
+  scaleBandFacet,
+  renderAxisX,
+  renderAxisY,
+  renderFacetsY,
+} from "./scaleBandFacet.js";
 import { yAxisLayout, xAxisLayout, summariseMutationsByGene } from "./utils.js";
 
 const data = [
@@ -32,7 +37,7 @@ const data = [
   { x: "Patient9", y: "TP53", type: "missense" },
 ];
 
-console.log(summariseMutationsByGene(data))
+console.log(summariseMutationsByGene(data));
 // debugger;
 const xOrder = [
   "Patient1",
@@ -58,7 +63,7 @@ const tmb = [
   { x: "Patient9", tmb: 6.2 },
 ];
 
-
+//prettier-ignore
 const annotations = [
   { x: "Patient1", annotationType1: "Annotation A", annotationType2: "Annotation X", numericAnnotation: 3.5 },
   { x: "Patient2", annotationType1: "Annotation B", annotationType2: "Annotation Y", numericAnnotation: 2.1 },
@@ -70,9 +75,6 @@ const annotations = [
   { x: "Patient8", annotationType1: "Annotation E", annotationType2: "Annotation S", numericAnnotation: 3.0 },
   { x: "Patient9", annotationType1: "Annotation D", annotationType2: "Annotation R", numericAnnotation: 7.1 },
 ];
-
-// Confidence level: high
-
 
 const yOrder = ["TP53", "RAD51", "BRCA1", "BRCA2"];
 const yFacets = ["TP53", "HRD", "HRD", "HRD"];
@@ -111,44 +113,44 @@ const width = window.innerWidth;
 const height = window.innerHeight;
 
 // Other tweakables
-const geneBarPadding = 0
-const geneBarWidth = 0
-const tickLength = 6
-const tickMarkAndTextPadding = 4
-const fontsizeFacet = 18
-const fontsizeY = 18
-const fontsizeX = 18
-const tmbBarPadding = 10
-const tmbBarHeight = 100
-const oncoplotClinicalPadding = 20
-const clinicalRowHeight = 30
-const clinicalRowPadding = 5
-const clinicalRowNumber = 5
-const showSampleNames = true
-
+const geneBarPadding = 0;
+const geneBarWidth = 0;
+const tickLength = 6;
+const tickMarkAndTextPadding = 4;
+const fontsizeFacet = 18;
+const fontsizeY = 18;
+const fontsizeX = 18;
+const tmbBarPadding = 10;
+const tmbBarHeight = 100;
+const oncoplotClinicalPadding = 20;
+const clinicalRowHeight = 30;
+const clinicalRowPadding = 5;
+const clinicalRowNumber = 5;
+const showSampleNames = true;
 
 //! Constant
 // Tooltip
-  const tooltip = d3
+const tooltip = d3
   .select("body")
   .append("div")
   .attr("class", "tooltip")
-  .style("opacity", 0)
+  .style("opacity", 0);
 
 const mousemove = function (event, d) {
   const x = event.pageX;
   const y = event.pageY;
-  
-  tooltip.style("left", x + "px")
+
+  tooltip
+    .style("left", x + "px")
     .style("top", y + "px")
     .style("opacity", 1)
     .html(d.tooltip);
-    tooltip.classed("active", true)
+  tooltip.classed("active", true);
 };
 
 const mouseleave = function () {
   tooltip.style("opacity", 0);
-  tooltip.classed("active", false)
+  tooltip.classed("active", false);
 };
 
 // Create SVG
@@ -168,54 +170,54 @@ const margin = {
 
 // Create Layout Objects
 const yLayout = yAxisLayout()
-.facets(yFacets)
-.domain(yOrder)
-.margin(margin)
-.windowWidth(width)
-.geneBarPadding(geneBarPadding)
-.geneBarWidth(geneBarWidth)
-.tickMarkAndTextPadding(tickMarkAndTextPadding)
-.tickLength(tickLength)
-.fontSizeFacet(fontsizeFacet)
-.fontSizeDomain(fontsizeY)
-.computeLayout()
+  .facets(yFacets)
+  .domain(yOrder)
+  .margin(margin)
+  .windowWidth(width)
+  .geneBarPadding(geneBarPadding)
+  .geneBarWidth(geneBarWidth)
+  .tickMarkAndTextPadding(tickMarkAndTextPadding)
+  .tickLength(tickLength)
+  .fontSizeFacet(fontsizeFacet)
+  .fontSizeDomain(fontsizeY)
+  .computeLayout();
 
 const xLayout = xAxisLayout()
-.domain(xOrder)
-.fontsizeX(fontsizeX)
-.showSampleNames(showSampleNames)
-.margin(margin)
-.windowHeight(height)
-.tmbBarPadding(tmbBarPadding)
-.tmbBarHeight(tmbBarHeight)
-.tickMarkAndTextPadding(tickMarkAndTextPadding)
-.oncoplotClinicalPadding(oncoplotClinicalPadding)
-.clinicalRowHeight(clinicalRowHeight)
-.clinicalRowPadding(clinicalRowPadding)
-.clinicalRowNumber(clinicalRowNumber)
-.tickLength(tickLength)
-.computeLayout()
+  .domain(xOrder)
+  .fontsizeX(fontsizeX)
+  .showSampleNames(showSampleNames)
+  .margin(margin)
+  .windowHeight(height)
+  .tmbBarPadding(tmbBarPadding)
+  .tmbBarHeight(tmbBarHeight)
+  .tickMarkAndTextPadding(tickMarkAndTextPadding)
+  .oncoplotClinicalPadding(oncoplotClinicalPadding)
+  .clinicalRowHeight(clinicalRowHeight)
+  .clinicalRowPadding(clinicalRowPadding)
+  .clinicalRowNumber(clinicalRowNumber)
+  .tickLength(tickLength)
+  .computeLayout();
 
-console.log(xLayout)
-console.log(yLayout)
-
+console.log(xLayout);
+console.log(yLayout);
 
 const yScale = scaleBandFacet()
-.range([xLayout.oncoplotPosStartY,xLayout.oncoplotPosEndY])
+  .range([xLayout.oncoplotPosStartY, xLayout.oncoplotPosEndY])
   .domain(yOrder)
   .facet(yFacets)
   .paddingInner(yPadding)
   .facetPaddingMultiplier(facetPaddingMultiplier);
 
-  const xScale = scaleBandFacet()
+const xScale = scaleBandFacet()
   .range([yLayout.oncoplotPosStartX, yLayout.oncoplotPosEndX])
   .domain(xOrder)
   .paddingInner(xPadding)
   .paddingOuter(xPaddingOuter);
 
-const yScaleTMB = d3.scaleLinear()
+const yScaleTMB = d3
+  .scaleLinear()
   .range([xLayout.tmbBarPosEndY, xLayout.tmbBarPosStartY])
-  .domain([0, d3.max(tmb.map(d => d.tmb))])
+  .domain([0, d3.max(tmb.map((d) => d.tmb))]);
 
 // ////////////////////////////////////////////////////////////////////////
 // // Create Marks Array
@@ -235,7 +237,7 @@ const marksTMB = tmb.map((d) => ({
   // color: getColor(typeAccessor(d)),
   // sample: xAccessor(d),
   // gene: yAccessor(d)
-  tooltip:  xAccessor(d) + "<br>" + "TMB: " + d.tmb ,
+  tooltip: xAccessor(d) + "<br>" + "TMB: " + d.tmb,
 }));
 
 const marksGeneBar = tmb.map((d) => ({
@@ -245,43 +247,47 @@ const marksGeneBar = tmb.map((d) => ({
   // color: getColor(typeAccessor(d)),
   // sample: xAccessor(d),
   // gene: yAccessor(d)
-  tooltip:  xAccessor(d) + "<br>" + "TMB: " + d.tmb ,
+  tooltip: xAccessor(d) + "<br>" + "TMB: " + d.tmb,
 }));
-
-
 
 // Render axes: Oncoplot
 renderAxisX(svg, xScale, xLayout.oncoplotPosEndY, showSampleNames, true, true);
-renderAxisY(svg, yScale, yLayout.oncoplotPosStartX, yLayout.facetWidth, yLayout.yTextAndTickWidth, true);
+renderAxisY(
+  svg,
+  yScale,
+  yLayout.oncoplotPosStartX,
+  yLayout.facetWidth,
+  yLayout.yTextAndTickWidth,
+  true
+);
 
 // Render axes: TMB
 const axisTMB = d3.axisLeft(yScaleTMB.nice(1)).ticks(1);
 svg
-.selectAll('.y-axis-tmb')
-.data([null])
-.join('g')
-.attr('class', 'y-axis-tmb')
-// .attr("transform", `translate(10, 10)`)
-.attr("transform", `translate(${yLayout.oncoplotPosStartX}, 0)`)
-.call(axisTMB)
-
+  .selectAll(".y-axis-tmb")
+  .data([null])
+  .join("g")
+  .attr("class", "y-axis-tmb")
+  // .attr("transform", `translate(10, 10)`)
+  .attr("transform", `translate(${yLayout.oncoplotPosStartX}, 0)`)
+  .call(axisTMB);
 
 // Render Marks: TMB
 svg
-.selectAll('.tmb-barplot')
-.data([null])
-.join("g")
-.attr('class', 'tmb-barplot')
-.selectAll("rect")
-.data(marksTMB)
-.join("rect")
-.attr("class", 'tmb-rect')
-.attr("x", (d) => d.xpos)
-.attr("y", (d) => d.ypos)
-.attr("width", xScale.bandwidth)
-.attr("height", (d) => d.height)
-.on("mousemove", mousemove)
-.on("mouseleave", mouseleave)
+  .selectAll(".tmb-barplot")
+  .data([null])
+  .join("g")
+  .attr("class", "tmb-barplot")
+  .selectAll("rect")
+  .data(marksTMB)
+  .join("rect")
+  .attr("class", "tmb-rect")
+  .attr("x", (d) => d.xpos)
+  .attr("y", (d) => d.ypos)
+  .attr("width", xScale.bandwidth)
+  .attr("height", (d) => d.height)
+  .on("mousemove", mousemove)
+  .on("mouseleave", mouseleave);
 
 // Render Marks: Oncoplot
 svg
@@ -304,17 +310,9 @@ svg
   .on("mousemove", mousemove)
   .on("mouseleave", mouseleave);
 
-
-// draw Oncoplot
-
-// Enforce fontsize
-
 // Font Size Enforcement
-svg.selectAll(".y-axis-tick-text")
-.style('font-size', fontsizeY + 'px')
+svg.selectAll(".y-axis-tick-text").style("font-size", fontsizeY + "px");
 
-svg.selectAll(".x-axis-tick-text")
-.style('font-size', fontsizeX + 'px')
+svg.selectAll(".x-axis-tick-text").style("font-size", fontsizeX + "px");
 
-svg.selectAll(".facet-text")
-.style('font-size', fontsizeFacet + 'px')
+svg.selectAll(".facet-text").style("font-size", fontsizeFacet + "px");
